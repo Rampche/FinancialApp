82f24498594fcefaefc27cb67ac3a8121f761d35
Gregorio Rampche <gregorioalmeida@gmail.com>
2024-05-28 20:57:27 -0300

    Adicionadas respostas.

--- Fina.Core/Responses/Response.cs
+++ Fina.Core/Responses/Response.cs
@@ -1,5 +1,32 @@
+using System
+
 namespace Fina.Core.Response
 
-public abstract class Response {
 
+
+public class Response<TData> {
+
+    private int _code = Configuration.DefaultStatusCode;
+
+    [JsonConstructor]
+    public Response() {
+        => _code = Configuration.DefaultStatusCode;
+
+    }
+
+    public Response(
+        TData? data, 
+        int code = Configuration.DefaultStatusCode,
+            string? message = null) 
+        {
+        Data = data;
+        _code = code;
+        Message = message;
+    }
+
+    public object? TData { get; set; }
+    public string? Message;
+
+    [JsonIgnore]
+    private bool IsSuccess => _code is >= 200 and <= 299;
 }
