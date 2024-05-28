3f7ad7bd83668af203f6436a63f1c818a0520b2b
Gregorio Rampche <gregorioalmeida@gmail.com>
2024-05-28 20:59:36 -0300

    Ajustes no arquivo de configurações

--- Fina.Core/Configuration.cs
+++ Fina.Core/Configuration.cs
@@ -2,7 +2,10 @@ namespace Fina.Core;
 
 public static class Configuration
 {
+    public const int DefaultStatusCode = 200;
+
     public const int DefaultPageNumber = 1;
+
     public const int DefaultPageSize = 25;
 
 }
